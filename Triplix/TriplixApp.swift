868e8374c64e5a6386ed5ca12b7dad9cfb6613db
Mark Martin <menm619@gmail.com>
2026-04-28 19:59:32 -0700

    Initial Commit

---
+++ Triplix/TriplixApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriplixApp.swift
+//  Triplix
+//
+//  Created by Mark Martin on 4/28/26.
+//
+
+import SwiftUI
+
+@main
+struct TriplixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
